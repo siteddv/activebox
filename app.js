08b44b1effcad8df1f571f63dddd3e570ebd3d21
Dimitrii <siteddv@gmail.com>
2021-04-07 23:32:46 +0600

    working with pop-up menu

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 $(function () {
 
+   // Fixed header
    let header = $("#header");
    let intro = $("#intro");
    let introHeight = intro.innerHeight();
    let scrollPos = $(window).scrollTop();
 
-   $(window).on("scroll", function () {
+   checkScroll(scrollPos, introHeight);
 
+   $(window).on("scroll resize", function () {
+
+      introHeight = intro.innerHeight();
       scrollPos = $(this).scrollTop();
 
+      checkScroll(scrollPos, introHeight);
+
+   });
+
+   function checkScroll(scrollPos, introHeight) {
       if (scrollPos > introHeight / 10) {
          header.addClass("fixed");
       } else {
          header.removeClass("fixed");
       }
+   }
+
+   // Smooth scroll
+   $("[data-scroll]").on("click", function (event) {
+      event.preventDefault();
+
+      let elementId = $(this).data('scroll');
+
+      let elementOffset = $(elementId).offset().top;
+
+      let headerHeight = $("#header").height();
+
+      $("html, body").animate({
+         scrollTop: elementOffset - headerHeight
+      }, 600);
 
    });
 
-   header.addClass("fixed");
 
 });
